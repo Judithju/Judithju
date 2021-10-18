8307ad29e20a8f5c137b94b9c56f4beb17432dd9
maarapan <maara.pan@gmail.com>
2021-10-18 12:35:11 +0200

    fixed changes

--- intro_workshop.R
+++ intro_workshop.R
@@ -8,9 +8,6 @@
 
 # coger del repositorio remoto los "archivos" (primer paso, ulyima version)
 
-<<<<<<< HEAD
 # this is what I added (JUDITH)
-=======
-## We add words by words, we learn, we drink coffee
 
->>>>>>> da641b3824ba9fabf0743f6c5fb7052ed4fa74a2
+## We add words by words, we learn, we drink coffee
