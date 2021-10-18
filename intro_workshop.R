6c41cf8e7d889d1ff7234f137f95af9eb4cd1c35
Judith <judith.sole.bertran@gmail.com>
2021-10-18 11:30:23 +0200

    fist stage

---
+++ intro_workshop.R
@@ -0,0 +1,10 @@
+# script for git workshop
+
+# git add <>: para pasar del directorio de trabajo al area de preparacion
+# commit: del staging area al lcoal repository
+# rastrear = captura de pantalla "commit"
+# pasos: working directory --> staging area --> local repository
+# push= compartir = repositorio remoto
+
+# coger del repositorio remoto los "archivos" (primer paso, ulyima version)
+
